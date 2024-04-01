328e9e5348300616415f19a6a881e38303512af2
kenzilai <kenzi.lai.dev@gmail.com>
2024-04-01 16:22:59 -0700

    ✨ feat: create Header

---
+++ portfolio/src/components/(header)/Header.tsx
@@ -0,0 +1,11 @@
+import { Navbar } from "./Navbar";
+import { Typewritter } from "./Typewritter";
+
+export function Header() {
+    return (
+        <header>
+            <Navbar />
+            <Typewritter />
+        </header>
+    );
+}
