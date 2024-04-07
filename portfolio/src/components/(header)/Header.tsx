2600b6d0427cd121d850c7b138239d2e592f7069
kenzilai <kenzi.lai.dev@gmail.com>
2024-04-07 00:08:37 -0700

    🔨 refactor: add div to Components

--- portfolio/src/components/(header)/Header.tsx
+++ portfolio/src/components/(header)/Header.tsx
@@ -6,9 +6,13 @@ export function Header() {
     return (
         <header>
             <Navbar />
-            <section className="flex flex-wrap-reverse items-center justify-around bg-black p-20">
-                <Typewritter />
-                <Avatar />
+            <section className="grid min-h-80 content-center justify-center bg-black sm:grid-cols-6">
+                <div className="order-last place-self-center sm:order-first sm:col-span-4">
+                    <Typewritter />
+                </div>
+                <div className="place-self-center sm:col-span-2 sm:place-self-start">
+                    <Avatar />
+                </div>
             </section>
         </header>
     );
