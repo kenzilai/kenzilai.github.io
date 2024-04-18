c7d8b38a1aad1973f0944f1e049d59a0aba4d1eb
kenzilai <kenzi.lai.dev@gmail.com>
2024-04-18 01:08:17 -0700

    chore: add responsive margin

--- portfolio/src/components/(header)/Typewritter.tsx
+++ portfolio/src/components/(header)/Typewritter.tsx
@@ -8,7 +8,7 @@ export function Typewritter() {
     const tagline = useTypedTagline(taglines);
 
     return (
-        <h1 className="blinking-cursor p-6 text-4xl font-extrabold text-white">
+        <h1 className="blinking-cursor mb-32 text-4xl font-extrabold text-white sm:m-6">
             Hi there! I'm
             <br />
             <span className="text-cyan-400">
