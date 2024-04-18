2df2c5266856e7cc1d0eb947f9962efecca39589
kenzilai <kenzi.lai.dev@gmail.com>
2024-04-18 01:08:03 -0700

    🧹 chore: add responsive margin

--- portfolio/src/components/(header)/Avatar.tsx
+++ portfolio/src/components/(header)/Avatar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export function Avatar() {
     return (
         <Image
-            className="border-white-500 m-5 rounded-full border-4"
+            className="border-white-500 mb-10 mt-32 rounded-full border-4 sm:m-5"
             src="/static/Avatar.png"
             width={150}
             height={150}
