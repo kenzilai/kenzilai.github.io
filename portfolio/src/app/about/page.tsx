b98f1e455395e582cde513344c079b6c10c39970
kenzilai <kenzi.lai.dev@gmail.com>
2024-04-17 23:36:49 -0700

    ✨ feat: create about

---
+++ portfolio/src/app/about/page.tsx
@@ -0,0 +1,41 @@
+export default function About() {
+    return (
+        <section id="about" className="container mx-auto px-20 pt-20">
+            <h2 className="text-center text-2xl">
+                <b>About</b>
+            </h2>
+            <main className="py-5 text-xl">
+                <p>
+                    My journey began with a curious mind in{" "}
+                    <b className="text-cyan-500">Game Development</b> and a
+                    thirst for knowledge in{" "}
+                    <b className="text-cyan-500">Programming</b>. Driven by my
+                    passion, I earned my bachelor's degree in Computer Graphics.
+                </p>
+                <br />
+                <p>
+                    <b>But I didn't stop there.</b>
+                </p>
+                <br />
+                <p>
+                    Recognizing that just the passion and knowledges in Computer
+                    Graphics alone weren't sufficient for my goals, so I
+                    enrolled in an intensive Coding Bootcamp. Through this
+                    experience, I discovered the joy of Web development and
+                    realized transferable nature of the programming skill.
+                </p>
+                <br />
+                <p>
+                    Being able to code has enabled me to pursue my interests in
+                    Web Development and contribute to Open Source projects while
+                    applying my passion to Game development.
+                </p>
+                <br />
+                <p>
+                    As you explore my website, I hope you will gain insight into
+                    who I am and the passion I bring to my work.
+                </p>
+            </main>
+        </section>
+    );
+}
