58bf0aee1ed4d6639a2d5ab58474dab92da7f155
kenzilai <kenzi.lai.dev@gmail.com>
2024-04-29 18:35:48 -0700

    🧹 chore: create hamburger menu

--- portfolio/src/components/(header)/Navbar.tsx
+++ portfolio/src/components/(header)/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export function Navbar() {
     return (
         <nav className="container relative mx-auto py-5">
-            <div className="flex items-center justify-center">
+            <div className="flex justify-end sm:items-center sm:justify-center">
                 <div className="hidden space-x-10 sm:flex">
                     <Link className="hover:text-cyan-400" href="#about">
                         About
@@ -39,6 +39,26 @@ export function Navbar() {
                             <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
                         </svg>
                     </Link>
+                </div>
+
+                {/* Hamburger Menu */}
+                <div className="px-10 sm:hidden">
+                    <button>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="h-8 w-8"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                            />
+                        </svg>
+                    </button>
                 </div>
             </div>
         </nav>
