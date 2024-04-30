281de0b4a9ea74641d33f1bb61e1fd0597781219
kenzilai <kenzi.lai.dev@gmail.com>
2024-04-29 18:41:24 -0700

    ✨ feat: add sections

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,9 +1,17 @@
 import { Header } from "@/components/(header)/Header";
+import About from "./about/page";
+import Skills from "./skills/page";
+import Projects from "./projects/page";
+import { Footer } from "@/components/(footer)/Footer";
 
 export default function Home() {
     return (
         <main>
             <Header />
+            <About />
+            <Skills />
+            <Projects />
+            <Footer />
         </main>
     );
 }
